38f5d1639f770274ecacb6f73bf43c3e9ed27683
Amanpawer <amanpawarap0912@gmail.com>
2025-07-23 09:13:35 -0600

    Add files via upload

---
+++ A2   task 01.py
@@ -0,0 +1,7 @@
+#thid code for find your number is odd or even
+print ("If you want to find out whether your number is odd or even, you need to enter your number here.")
+A =int(input("enter your number :"))
+if A%2 == 0:
+    print(A,"is an even  number.")
+else:
+    print(A,"is an odd  number.")
---
+++ A2 task 02.py
@@ -0,0 +1,4 @@
+
+for A in range(int(input("enter your number",))):
+    print(A)
+    print(A*A/2+A/2)
