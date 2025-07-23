fe5a08a3e57e2f830866d7298ec6e3c224418452
Amanpawer <amanpawarap0912@gmail.com>
2025-07-23 09:16:21 -0600

    Update A2 task 02.py

--- A2 task 02.py
+++ A2 task 02.py
@@ -1,4 +1,5 @@
-
+#all number sum in one
+print ("if you want to sum of 1 to 100 just enter 101 any your ens is ther")
 for A in range(int(input("enter your number",))):
     print(A)
     print(A*A/2+A/2)
